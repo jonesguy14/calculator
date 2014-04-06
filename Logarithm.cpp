4ffe61abef451ce19ce1aa5ea5fa9e5bb2beb391
jamesmsteele <jamessteele90@gmail.com>
2014-04-06 18:29:27 -0400

    Update Logarithm.cpp

--- Logarithm.cpp
+++ Logarithm.cpp
@@ -1,14 +1,28 @@
 #include "Logarithm.h"
+#include <cmath>
 using namespace std;
 
-Logarithm::Logarithm(MathematicalExpression base, MathematicalExpression exponent){
-	if(exponent.toDecimal < 0){
+Logarithm::Logarithm(Decimal base, Decimal exponent){
+	if(exponent.toDecimal() < 0){
 		throw "Cannot have the logarithm of a negative number";	
 	}
-	if(exponent.toDecimal == 0){
+	if(exponent.toDecimal() == 0){
 		throw "Cannot have the logarithm of 0";
 	}
-	if(exponent.toDecimal == 1){
+	if(exponent.toDecimal() == 1){
+		throw "Logarithm is equal to 0";	
+	}
+	throw "Logarithm is solvable; use simple evaluation.";
+}
+
+Logarithm::Logarithm(MathematicalExpression base, MathematicalExpression exponent){
+	if(exponent.toDecimal() < 0){
+		throw "Cannot have the logarithm of a negative number";	
+	}
+	if(exponent.toDecimal() == 0){
+		throw "Cannot have the logarithm of 0";
+	}
+	if(exponent.toDecimal() == 1){
 		throw "Logarithm is equal to 0";	
 	}
 	this->base		=	base.simplify();
