b00db771aa1c011308e2e002c16ce13042297698
Achi Jones <jonesguy14@gmail.com>
2014-04-09 00:09:07 -0400

    tweaked print on history, made help look cooler

--- Driver.cpp
+++ Driver.cpp
@@ -43,10 +43,10 @@ int main()
         case 'A':
             while (keep_compute) {
                 P.last_ans = historian.ans();
-                cout << "\nEnter your input: (enter 'BACK' to return to menu)" << endl;
+                cout << "\nEnter your input: (enter 'BACK' or 'QUIT' to return to menu)" << endl;
                 cout << "INPUT: ";
                 getline(cin, input);
-                if (input.find("BACK") != string::npos) {
+                if (input.find("BACK") != string::npos || input.find("back") != string::npos || input.find("QUIT") != string::npos || input.find("quit") != string::npos) {
                     //user wants to quit out
                     keep_compute = false;
                 }
@@ -68,23 +68,28 @@ int main()
             break;
         case 'h':
         case 'H':
-            cout << "\nThis \"intelligent\" calculator will take your input and calculate a solution,\n"
+            cout << "\n                            * * * H E L P * * *" << endl;
+            cout << "--------------------------------------------------------------------------------" << endl;
+            cout << "This \"intelligent\" calculator will take your input and calculate a solution,\n"
                  << "avoiding floating point representation and providing an answer fit for a math\n"
-                 << "textbook.\n" << endl;
-            cout << "          ***HOW TO USE:***" << endl;
-            cout << "---------------------------------------------------------" << endl;
-            cout << "OPERATION DESIRED                      |   WHAT TO INPUT  " << endl;
-            cout << "---------------------------------------------------------" << endl;
-            cout << " Add x to y                            |      x + y       " << endl;
-            cout << " Subtract y from x                     |      x - y       " << endl;
-            cout << " Multiply x and y                      |      x * y       " << endl;
-            cout << " Divide x by y                         |      x / y       " << endl;
-            cout << " Calculate x to the power of y         |      x ^ y       " << endl;
-            cout << " Take the square root of x             |      sqrt:x      " << endl;
-            cout << " Take the nth root of x                |      nrt:x       " << endl;
-            cout << " Take the log base b of x              |      log_b:x     " << endl;
-            cout << " Use the previous answer               |      ans         " << endl;
-            cout << "---------------------------------------------------------" << endl;
+                 << "textbook.\n"
+                 << "\nThis calculator knows the order of operations and can handle long, convoluted\n"
+                 << "expressions with many terms and parentheses.\n"
+                 << "For example, 3 ^ 7 + 2 * ( sqrt:64 / 4 + 10 ) + log_5:125 is acceptable.\n\n";
+            cout << "                            * * * HOW TO USE: * * *" << endl;
+            cout << "--------------------------------------------------------------------------------";
+            cout << "OPERATION DESIRED                      |  WHAT TO INPUT  |        EXAMPLE       ";
+            cout << "--------------------------------------------------------------------------------";
+            cout << " Add x to y                            |      x + y      |        5 + 7" << endl;
+            cout << " Subtract y from x                     |      x - y      |        12 - 6" << endl;
+            cout << " Multiply x and y                      |      x * y      |        8 * 15" << endl;
+            cout << " Divide x by y                         |      x / y      |        34 / 2" << endl;
+            cout << " Calculate x to the power of y         |      x ^ y      |        3 ^ 7" << endl;
+            cout << " Take the square root of x             |      sqrt:x     |        sqrt:49" << endl;
+            cout << " Take the nth root of x                |      nrt:x      |        3rt:27" << endl;
+            cout << " Take the log base b of x              |      log_b:x    |        log_5:625" << endl;
+            cout << " Use the previous answer               |      ans        |        ans" << endl;
+            cout << "--------------------------------------------------------------------------------" << endl;
             break;
         case 'q':
         case 'Q':
