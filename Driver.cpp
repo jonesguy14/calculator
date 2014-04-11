9d7f1e9be9f67f4db66c26a42337195edb579193
Achi Jones <jonesguy14@gmail.com>
2014-04-11 14:21:14 -0400

    allowed user to type in 'help' to show help

--- Driver.cpp
+++ Driver.cpp
@@ -5,6 +5,31 @@
 
 using namespace std;
 
+void displayHelp() {
+    cout << "\n                              * * * H E L P * * *" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+    cout << "This \"intelligent\" calculator will take your input and calculate a solution,\n"
+         << "avoiding floating point representation and providing an answer fit for a math\n"
+         << "textbook.\n"
+         << "\nThis calculator knows the order of operations and can handle long, convoluted\n"
+         << "expressions with many terms and parentheses.\n"
+         << "For example, 3 ^ 7 + 2 * ( sqrt:64 / 4 + 10 ) + log_5:125 is acceptable.\n\n";
+    cout << "                            * * * HOW TO USE: * * *" << endl;
+    cout << "--------------------------------------------------------------------------------";
+    cout << "OPERATION DESIRED                      |  WHAT TO INPUT  |        EXAMPLE       ";
+    cout << "--------------------------------------------------------------------------------";
+    cout << " Add x to y                            |      x + y      |        5 + 7" << endl;
+    cout << " Subtract y from x                     |      x - y      |        12 - 6" << endl;
+    cout << " Multiply x and y                      |      x * y      |        8 * 15" << endl;
+    cout << " Divide x by y                         |      x / y      |        34 / 2" << endl;
+    cout << " Calculate x to the power of y         |      x ^ y      |        3 ^ 7" << endl;
+    cout << " Take the square root of x             |      sqrt:x     |        sqrt:49" << endl;
+    cout << " Take the nth root of x                |      nrt:x      |        3rt:27" << endl;
+    cout << " Take the log base b of x              |      log_b:x    |        log_5:625" << endl;
+    cout << " Use the previous answer               |      ans        |        ans" << endl;
+    cout << "--------------------------------------------------------------------------------" << endl;
+}
+
 int main()
 {
     bool gogo = true;
@@ -43,12 +68,15 @@ int main()
         case 'A':
             while (keep_compute) {
                 P.last_ans = historian.ans();
-                cout << "\nEnter your input: (enter 'BACK' or 'QUIT' to return to menu)" << endl;
+                cout << "\nEnter your input: (enter 'BACK' or 'QUIT' to return to menu, or enter 'HELP' to show help)" << endl;
                 cout << "INPUT: ";
                 getline(cin, input);
                 if (input.find("BACK") != string::npos || input.find("back") != string::npos || input.find("QUIT") != string::npos || input.find("quit") != string::npos) {
                     //user wants to quit out
                     keep_compute = false;
+                }
+                else if (input.find("HELP") != string::npos || input.find("help") != string::npos) {
+                    displayHelp();
                 }
                 else {
                     //continue to calculate
@@ -74,28 +102,7 @@ int main()
             break;
         case 'h':
         case 'H':
-            cout << "\n                              * * * H E L P * * *" << endl;
-            cout << "--------------------------------------------------------------------------------" << endl;
-            cout << "This \"intelligent\" calculator will take your input and calculate a solution,\n"
-                 << "avoiding floating point representation and providing an answer fit for a math\n"
-                 << "textbook.\n"
-                 << "\nThis calculator knows the order of operations and can handle long, convoluted\n"
-                 << "expressions with many terms and parentheses.\n"
-                 << "For example, 3 ^ 7 + 2 * ( sqrt:64 / 4 + 10 ) + log_5:125 is acceptable.\n\n";
-            cout << "                            * * * HOW TO USE: * * *" << endl;
-            cout << "--------------------------------------------------------------------------------";
-            cout << "OPERATION DESIRED                      |  WHAT TO INPUT  |        EXAMPLE       ";
-            cout << "--------------------------------------------------------------------------------";
-            cout << " Add x to y                            |      x + y      |        5 + 7" << endl;
-            cout << " Subtract y from x                     |      x - y      |        12 - 6" << endl;
-            cout << " Multiply x and y                      |      x * y      |        8 * 15" << endl;
-            cout << " Divide x by y                         |      x / y      |        34 / 2" << endl;
-            cout << " Calculate x to the power of y         |      x ^ y      |        3 ^ 7" << endl;
-            cout << " Take the square root of x             |      sqrt:x     |        sqrt:49" << endl;
-            cout << " Take the nth root of x                |      nrt:x      |        3rt:27" << endl;
-            cout << " Take the log base b of x              |      log_b:x    |        log_5:625" << endl;
-            cout << " Use the previous answer               |      ans        |        ans" << endl;
-            cout << "--------------------------------------------------------------------------------" << endl;
+            displayHelp();
             break;
         case 'q':
         case 'Q':
