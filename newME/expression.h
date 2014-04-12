99c986fbc79fe6cc363150b5db3ce868612e91ad
jamesmsteele <jamessteele90@gmail.com>
2014-04-12 16:21:15 -0400

    Update and rename Expression.h to expression.h

---
+++ newME/expression.h
@@ -0,0 +1,33 @@
+#ifndef EXPRESSION_H_INCLUDED
+#define EXPRESSION_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+
+class Expression{
+	private:
+		Expression* value;
+		bool initialized;
+		std::vector<Expression*> addition;
+		std::vector<Expression*> multiplication;
+
+	public:
+		Expression(Expression*);
+		Expression(std::vector<Expression*>);
+		Expression();
+		~Expression();
+
+		void add(Expression*);
+		void subtract(Expression*);
+		void multiply(Expression*);
+		void divide(Expression*);
+		void exponentiate(Expression*);
+		void negative();
+		void add_simplify(std::vector<Expression*>);
+		void multiply_simplify(std::vector<Expression*>);
+		void simplify();
+
+		std::string toString();
+};
+
+#endif // EXPRESSION_H_INCLUDED
