bff4cdab2245c6f58b4a61d98402cc5d393caf92
A18502 <a18502@ufl.edu>
2014-04-12 18:10:57 -0400

    Create exceptions.h

---
+++ exceptions.h
@@ -0,0 +1,27 @@
+#ifndef EXCEPTIONS_H_
+#define EXCEPTIONS_H_
+
+#include <string>
+using namespace std;
+
+class Exceptions {
+	//Values
+	int errorID;
+	string errorName;
+
+	//Constructor
+	Exceptions();
+	Exceptions(string errorName);
+
+	//Destructor
+	~Exceptions() {
+	};
+
+	//Methods
+	int determineError(string errorName);
+
+
+
+};
+
+#endif /* EXCEPTIONS_H_ */
