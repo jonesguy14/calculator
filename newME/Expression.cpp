d3172ab51ea4e241f3ae8f1d430e0b8c3b46db03
jamesmsteele <jamessteele90@gmail.com>
2014-04-12 16:06:11 -0400

    Create Expression.cpp

---
+++ newME/Expression.cpp
@@ -0,0 +1,181 @@
+#include <vector>
+
+Expression::Expression(Expression* exp){
+	/*
+		If we only want to add one expression, we'll set value equal to the expression and set initialized to false.
+		This will tell our other methods to move value to the first slot of the appropriate vector.
+	*/
+	this->value	=	exp;
+	this->initialized	=	false;
+}
+Expression::Expression(){
+	/*
+		If we want to create an empty expression, we'll set initialized to true to indicate that value doesn't need
+		to be placed in the first slot of a vector when add() or multiply() is called.
+	*/
+	this->initialized	=	true;
+}
+Expression::Expression(std::vector<Expression*> addition){
+	/*
+		In some cases, we'll want to accept an addition vector, which allows us to multiply. More information will be
+		in our multiply() method.
+	*/
+	this->initiliazed	=	true;
+	this->addition		=	addition;
+}
+void Expression::add(Expression* addend){
+	/*
+		If initialized is true, we don't need to worry about the value variable.
+	*/
+	if(this->initialized){
+		this->addition.push_back(addend);
+	}
+	/*
+		Otherwise, we need to set it to true, put it in our addition vector, then conduct business as usual.
+	*/
+	else{
+		this->initiliazed	=	true;
+		this->addition.push_back(value);
+		this->addition.push_back(addend);
+	}
+}
+void Expression::subtract(Expression* subtrahend){
+	/*
+		We call negative() on our subtrahend so that we can add it to our addition vector.
+		This allows us to avoid having a vector just for subtraction, which makes our lives a lot easier.
+	*/
+	this->add(subtrahend->negative());
+}
+void Expression::multiply(Expression* multiplicand){
+	/*
+		Like in the addition method, we have to handle our value variable.
+	*/
+	if(!this->initialized){
+		this->initiliazed	=	true;
+		this->multiplication.push_back(value);
+	}
+	/*
+		If our expression has an addition vector, we need to put it in our multiplication vector.
+		For example, if we multiply "log_5:10 +  e^5" by "pi^6", we would take [log_5:10*, e^5*], turn it into
+		an Expression object via the vector constructor, then we'd have a multiplication vector of [[log_5:10*, e^5*], pi^6].
+	*/
+	if(!this->addition.empty()){}
+		this->add_simplify(this->addition);
+		Expression* exp	=	new Expression(this->addition);
+		this->addition.clear();
+		this->multiplication.push_back(exp);
+	}
+	this->multiplication.push_back(multiplicand);
+}
+void Expression::divide(Expression* dividend){
+	/*
+		Instead of implementing a division vector, it's easier to just multiply by the reciprocal.
+	*/
+	Fraction* reciprocal	=	new Fraction(1, *dividend);
+	this->multiply(reciprocal);
+}
+void Expression::exponentiate(Expression* exponent){
+	/*
+		We don't need to handle exponents. It's easier to just do Exponent(Expression, exponent).
+	*/
+	throw "Use exponent class instead";
+}
+void Expression::add_simplify(std::vector<Expression*> exp){
+	/*
+		Ideally, we wouldn't go through this vector linearly,
+		but it's unlikely that the size is very large, so it shouldn't be that bad.
+	*/
+	/*
+		We seek through our vector and add anything together that can be added together.
+		For example, [5, log_5:10, 10] would become [15, log_5:10].
+	*/
+	int c	=	exp->size;
+	for(int i = 0; i < c; i++){
+		for(j = 0; j < c; j++){
+			try{
+				exp[i]->add(exp[j]);
+				exp->erase(j);
+			}catch(ExpressionException e){
+				continue;
+			}
+		}
+	}
+}
+void Expression::multiply_simplify(std::vector<Expression*> exp){
+	/*
+		Just like the add_simplify() method, except with multiplication.
+	*/
+	int c	=	exp->size();
+	for(int i = 0; i < c; i++){
+		for(j = 0; j < c; j++){
+			try{
+				exp[i]->multiply(exp[j]);
+				exp->erase(j);
+			}catch(ExpressionException e){
+				continue;
+			}
+		}
+	}
+}
+void Expression::simplify(){
+	/*
+		Just simplify both vectors and call it a day
+	*/
+	this->add_simplify(this->addition);
+	this->multiply_simplify(this->multiplication);
+}
+
+std::string Expression::toString(){
+	/*
+		If we only have our initial value, let's just return it in string form.
+	*/
+	if(!this->initialized){
+		return this->value.toString();
+	}
+	
+	int c	=	this->addition.size();
+	std::string	result	=	"";
+	
+	/*
+		Handle addition
+	*/
+	for(int i = 0; i < c; i++){
+		/*
+			If we have multiple addition elements, it's a good idea to wrap them in parentheses.
+		*/
+		if(i == 0 && c > 1){
+			result += "(";
+		}
+		result	+=	this->addition[i].toString();
+		/*
+			Add an addition symbol after our element as long as it isn't the last one in the vector.
+		*/
+		if(i < (c - 1)){
+			result += " + ";
+		}
+		/*
+			Close the parentheses when we're done.
+		*/
+		if(i == (c - 1) && c > 1){
+			result += ")";
+		}
+	}
+	
+	/*
+		If we have something to multiply by, put a multiplication symbol in between the two pieces of our string.
+	*/
+	if(!this->multiplication.empty()){
+		result += " * ";
+	}
+	
+	int c	=	this->multiplication.size();
+	
+	for(int i = 0; i < c; i++){
+		result	+=	this->multiplication[i].toString();
+		if(i < (c - 1)){
+			result += " * ";
+		}
+	}
+	return result;
+}
+
