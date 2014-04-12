fe9c729b634a3b11a7379553f9e345332243edb7
jamesmsteele <jamessteele90@gmail.com>
2014-04-12 18:18:04 -0400

    Update and rename Logarithm.h to logarithm.h

--- newME/logarithm.h
+++ newME/logarithm.h
@@ -1,27 +1,39 @@
-#include "Expression.h"
+#ifndef LOGARITHM_H_INCLUDED
+#define LOGARITHM_H_INCLUDED
+
+#include "expression.h"
 #include <iostream>
+#include <vector>
 
 class Logarithm : public Expression{
 	private:
-		Expression base;
-		Expression argument;
-		
+		std::vector<Expression> coefficient;
+		std::vector<Expression>  base;
+		std::vector<Expression>  argument;
+
 	public:
-		Logarithm(Expression, Expression);
-		Logarithm(Expression, Exponent); // Check if base == Exponent.base, throws error: Simple logarithm
-		Logarithm(Integer, Integer); // If evaluates to a clean number, throws error: Simple expression
-		
+		Logarithm(Expression, Expression, Expression);
+		Logarithm(Expression, Expression, Exponent); // Check if base == Exponent.base, throws error: Simple logarithm
+		Logarithm(Expression, MathExInteger, MathExInteger); // If evaluates to a clean number, throws error: Simple expression
+
 		void add(Expression*); // Throws error: Cannot add expression to logarithm
 		void subtract(Expression*); // Throws error: Cannot subtract expression from logarithm
 		void multiply(Expression*); // Throws error: Cannot multiply expression with logarithm
 		void multiply(Logarithm*); // Throws error: Cannot multiply logarithms
 		void divide(Expression*); // Throws error: Cannot divide logarithm by expression
-		
+
 		void add(Logarithm*);
 		void subtract(Logarithm*);
 		void divide(Logarithm*);
-		
+
 		void simplify();
 		std::string toString();
 		double toDecimal();
+		
+		Expression getCoefficient();
+		Expression getBase();
+		Expression getArgument();
 }
+
+#endif // LOGARITHM_H_INCLUDED
+				
