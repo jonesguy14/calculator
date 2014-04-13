92cb3611ed2f846161dde640ee1371d55ca22365
jamesmsteele <jamessteele90@gmail.com>
2014-04-13 18:33:15 -0400

    Update pi.cpp

--- newME/pi.cpp
+++ newME/pi.cpp
@@ -1,66 +1,32 @@
-#include "pi.h"
+#ifndef PI_H_INCLUDED
+#define PI_H_INCLUDED
 
-Pi::Pi() {
-    MathExInteger* co	=	new MathExInteger(1);
-    this->coefficient.push_back(co);
-}
+#include "constant.h"
+#include "exception.h"
+#include "exp-integer.h"
+#include <iostream>
+#include <vector>
+
+class Pi : public Constant {
+private:
+	std::vector<Expression*> coefficient;
+public:
+    Pi();
+    ~Pi();
+
+    void add(Pi* a);
+    void add(Expression* a);
+    void subtract(Pi* s);
+    void subtract(Expression* s);
+    void multiply(Expression* m);
+    void divide(Expression* d);
+    void negative();
+
+    std::string getName();
+    std::string toString();
+    double toDecimal();
+    Expression* getCoefficient();
+};
 
 
-std::string Pi::getName() {
-    return "Pi";
-}
-
-std::string Pi::toString() {
-    return this->getCoefficient()->toString() + "pi";
-}
-
-double Pi::toDecimal() {
-    return 3.1415926535 * this->getCoefficient()->toDecimal();
-}
-
-Expression* Pi::getCoefficient() {
-    return this->coefficient.back();
-}
-
-void Pi::add(Expression* a) {
-    throw Exceptions("Pi cannot add that data type.");
-}
-
-void Pi::subtract(Expression* s) {
-    throw Exceptions("Pi cannot subtract that data type.");
-}
-
-void Pi::multiply(Expression* m) {
-    throw Exceptions("Pi cannot multiply that data type.");
-}
-
-void Pi::divide(Expression* d) {
-    throw Exceptions("Pi cannot divide that data type.");
-}
-
-void Pi::add(Pi* a) {
-    try {
-        this->getCoefficient()->add(a->getCoefficient());
-    }
-    catch (Exceptions e) {
-        Expression* exp = new Expression(this->getCoefficient());
-        exp->add(a->getCoefficient());
-        this->coefficient.push_back(exp);
-    }
-}
-
-void Pi::subtract(Pi* s) {
-    try {
-        this->getCoefficient()->subtract(s->getCoefficient());
-    }
-    catch (Exceptions e) {
-        Expression* exp = new Expression(this->getCoefficient());
-        exp->subtract(s->getCoefficient());
-        this->coefficient.push_back(exp);
-    }
-}
-
-void Pi::negative() {
-    MathExInteger* neg = new MathExInteger(-1);
-    this->getCoefficient()->multiply(neg);
-}
+#endif // PI_H_INCLUDED
