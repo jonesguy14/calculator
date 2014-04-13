2caf2b22f6f55cbe5088bdfa7b2b16f95f82d06d
jamesmsteele <jamessteele90@gmail.com>
2014-04-13 14:10:31 -0400

    Create euler.h

---
+++ newME/euler.h
@@ -0,0 +1,22 @@
+#ifndef EULERS_H_INCLUDED
+#define EULERS_H_INCLUDED
+
+#include <iostream>
+
+class Eulers : public Constant {
+public:
+    Eulers();
+    ~Eulers();
+
+    Expression* add(Expression* a);
+    Expression* subtract(Expression* s);
+    Expression* multiply(Expression* m);
+    Expression* divide(Expression* d);
+
+    std::string getName();
+    std::string toString();
+    double toDecimal();
+    MathExInteger* coeffecient;
+};
+
+#endif // EULERS_H_INCLUDED
