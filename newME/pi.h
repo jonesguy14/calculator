57f5496f723c533b002bef8152c93358f659f308
jamesmsteele <jamessteele90@gmail.com>
2014-04-13 15:34:51 -0400

    Update pi.h

--- newME/pi.h
+++ newME/pi.h
@@ -1,30 +1,61 @@
-#ifndef PI_H_INCLUDED
-#define PI_H_INCLUDED
+#include "Pi.h"
 
-#include "constant.h"
-#include "exception.h"
-#include <iostream>
-#include <vector>
+Pi::Pi() {
+	MathExInteger i(1);
+    this->coeffecient.push_back(i);
+}
 
-class Pi : public Constant {
-private:
-	std::vector<Expression> coefficient;
-public:
-    Pi();
-    ~Pi();
+Pi::~Pi() {
+	delete[] this->coefficient;
+    delete this;
+}
 
-    void add(Pi* a);
-    void add(Expression* a);
-    void subtract(Pi* s);
-    void subtract(Expression* s);
-    void multiply(Expression* m);
-    void divide(Expression* d);
+std::string Pi::getName() {
+    return "Pi";
+}
 
-    std::string getName();
-    std::string toString();
-    double toDecimal();
-    Expression getCoefficient();
-};
+std::string Pi::toString() {
+    return this->getCoefficient()->toString() << "pi";
+}
 
+double Pi::toDecimal() {
+    return 3.1415926535;
+}
 
-#endif // PI_H_INCLUDED
+void Pi::add(Pi* a){
+	try{
+		this->getCoefficient()->add(a->getCoefficient());
+	}catch(Exceptions e){
+		Expression* exp	=	new Expression(a->getCoefficient());
+		exp->add(this->getCoefficient());
+		this->coefficient.push_back(exp);
+	}
+}
+
+void Pi::add(Expression* a) {
+    throw Exceptions("Cannot add a constant to that type");
+}
+void Pi::subtract(Pi* s) {
+	try{
+		this->getCoefficient()->subtract(s->getCoefficient());
+	}catch(Exceptions e){
+		Expression* exp	=	new Expression(s->getCoefficient());
+		exp->subtract(this->getCoefficient());
+		this->coefficient.push_back(exp);
+	}
+}
+void Pi::subtract(Expression* a) {
+    throw Exceptions("Cannot subtract a constant from that type");
+}
+
+void Pi::multiply(Expression* m) {
+    throw Exceptions("Cannot multiply a constant with that type");
+}
+
+void Pi::divide(Expression* d) {
+    throw Exceptions("Cannot divide a constant by that type");
+}
+
+Expression* Pi::getCoefficient(){
+	return this->coefficient.back();
+}
