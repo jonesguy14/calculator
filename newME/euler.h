42be1252ccd8091e0bd3c1bb5e6fa39d1ff0ffe2
jonesguy14 <jonesguy14@gmail.com>
2014-04-13 15:47:23 -0400

    Update euler.h

--- newME/euler.h
+++ newME/euler.h
@@ -2,10 +2,16 @@
 #define EULERS_H_INCLUDED
 
 #include "constant.h"
+#include "exception.h"
 #include "exp-integer.h"
 #include <iostream>
+#include <vector>
+
 
 class Eulers : public Constant {
+private:
+    std::vector<Expression*> coefficient;
+
 public:
     Eulers();
     ~Eulers();
@@ -14,11 +20,16 @@ public:
     void subtract(Expression* s);
     void multiply(Expression* m);
     void divide(Expression* d);
+    void add(Eulers* a);
+    void subtract(Eulers* s);
+    void multiply(Eulers* m);
+    void divide(Eulers* d);
+
+    Expression* getCoefficient();
 
     std::string getName();
     std::string toString();
     double toDecimal();
-    MathExInteger* coefficient;
 };
 
 #endif // EULERS_H_INCLUDED
