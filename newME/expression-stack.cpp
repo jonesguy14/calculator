957fa33f86c5089a1d823d0df0036415e2635445
jamesmsteele <jamessteele90@gmail.com>
2014-04-13 14:16:55 -0400

    Create expression-stack.cpp

---
+++ newME/expression-stack.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "expression.h"
+
+using namespace std;
+
+class expressionStack {
+
+private:
+   int MAX;
+   int top;
+   Expression* items;
+
+public:
+	expressionStack(int size){
+		MAX = size;
+		top = -1;
+		items = new Expression[MAX];
+	}
+
+	~expressionStack(){ delete [] items; }
+
+	void push(Expression expression){
+		if(isFull()){
+			cout << "Stack Full!" << endl;
+		}
+
+		items[++top] = expression;
+	}
+
+	Expression pop(){
+		if (isEmpty()){
+			throw "Error with input! Operator to term ratio does not match up.";
+		}
+        else {
+            return items[top--];
+        }
+	}
+
+	Expression getTop() {
+        return items[top];
+	}
+
+	bool hasItems() {
+        if (top>-1) {return true;}
+        else return false;
+	}
+
+	int isEmpty(){ return top == -1; }
+
+	int isFull(){ return top+1 == MAX; }
+};
