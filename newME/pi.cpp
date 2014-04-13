e052a5be741f0f9f2e4ac37c8c08b693de47cb19
jamesmsteele <jamessteele90@gmail.com>
2014-04-13 15:25:01 -0400

    Update pi.cpp

--- newME/pi.cpp
+++ newME/pi.cpp
@@ -1,46 +1,60 @@
 #include "Pi.h"
 
-using namespace std;
-
 Pi::Pi() {
-    this->coeffecient = new MathExInteger(1);
+	MathExInteger i(1);
+    this->coeffecient.push_back(i);
 }
 
 Pi::~Pi() {
     delete this;
 }
 
-string Pi::getName() {
+std::string Pi::getName() {
     return "Pi";
 }
 
-string Pi::toString() {
-    stringstream s;
-    s << this->coefficient << "pi";
-    return s.str();
+std::string Pi::toString() {
+    return this->getCoefficient().toString() << "pi";
 }
 
 double Pi::toDecimal() {
     return 3.1415926535;
 }
 
-Expression* Pi::add(Expression* a) {
-    if a->getName() == "Pi" {
-        coefficient = new MathExInteger(a->coefficient->getInt() + coefficient->getInt());
-        return this;
-    }
+void Pi::add(Pi* a){
+	try{
+		this->getCoefficient().add(a->getCoefficient());
+	}catch(Exceptions e){
+		Expression exp(a->getCoefficient());
+		exp.add(this->getCoefficient());
+		this->coefficient.push_back(exp);
+	}
 }
 
-Expression* Pi::subtract(Expression* s) {
-    if a->getName() == "Pi" {
-        coefficient = new MathExInteger(coefficient->getInt() - s->coefficient->getInt());
-        return this;
-    }
+void Pi::add(Expression* a) {
+    throw Exceptions("Cannot add a constant to that type");
+}
+void Pi::subtract(Pi* s) {
+	try{
+		this->getCoefficient().subtract(s->getCoefficient());
+	}catch(Exceptions e){
+		Expression exp(this->getCoefficient());
+		exp.subtract(s->getCoefficient());
+		this->coefficient.push_back(exp);
+	}
+}
+void Pi::subtract(Expression* a) {
+    throw Exceptions("Cannot subtract a constant from that type");
 }
 
-Expression* Pi::multiply(Expression* m) {
-    if a->getName() == "Pi" {
-        coefficient = new MathExInteger(m->coefficient->getInt() * coefficient->getInt());
-        return this;
-    }
+void Pi::multiply(Expression* m) {
+    throw Exceptions("Cannot multiply a constant with that type");
+}
+
+void Pi::divide(Expression* d) {
+    throw Exceptions("Cannot divide a constant by that type");
+}
+
+Expression Pi::getCoefficient(){
+	return this->coefficient.back();
 }
